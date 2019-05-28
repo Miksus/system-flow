2a008420cd34e5c3adfbd13ddf69e246b9f42845
Mikael Koli <koli.mikael@gmail.com>
2019-05-28 20:06:47 +0300

    ADD: tests for stock construct

--- tests/test_stock/test_construct.py
+++ tests/test_stock/test_construct.py
@@ -54,3 +54,42 @@ def test_to_flow():
     # Test the flow did not change values just in case
     assert a.value == 2
     assert b.value == 8
+
+def test_to_flow_rev():
+    a = sf.Stock("A", initial_value=2, lower_limit=0, upper_limit=10)
+    b = sf.Stock("B", initial_value=8, lower_limit=0, upper_limit=10)
+
+    new = b << a
+    assert isinstance(new, sf.Flow)
+    # Test the flow has input & output correctly
+    assert new.input is a
+    assert new.output is b
+    # Test the flow did not change values just in case
+    assert a.value == 2
+    assert b.value == 8
+
+
+def test_to_inflow():
+    a = sf.Stock("A", initial_value=2, lower_limit=0, upper_limit=10)
+    new = a << None
+    assert isinstance(new, sf.flow.InFlow)
+    assert 2 == a.value
+
+def test_to_outflow():
+    b = sf.Stock("B", initial_value=2, lower_limit=0, upper_limit=10)
+    new = b >> None
+    assert isinstance(new, sf.flow.OutFlow)
+    assert 2 == b.value
+
+
+def test_to_inflow_rev():
+    a = sf.Stock("A", initial_value=2, lower_limit=0, upper_limit=10)
+    new = None >> a
+    assert isinstance(new, sf.flow.InFlow)
+    assert 2 == a.value
+
+def test_to_outflow_rev():
+    b = sf.Stock("B", initial_value=2, lower_limit=0, upper_limit=10)
+    new = None << b
+    assert isinstance(new, sf.flow.OutFlow)
+    assert 2 == b.value
