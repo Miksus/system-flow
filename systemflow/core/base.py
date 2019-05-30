da93fe0b85ff66a8ee0c371daf948dd25da69f72
Mikael Koli <koli.mikael@gmail.com>
2019-05-30 11:11:36 +0300

    ADD: reseting all sub elements to master base, UPD: property for sub elements

--- systemflow/core/base.py
+++ systemflow/core/base.py
@@ -1,6 +1,6 @@
 
 from collections import Iterable
-from systemflow.visuals import Graphviz
+from systemflow import visuals
 
 class SimBase:
 
@@ -8,7 +8,7 @@ class SimBase:
     Base class for all sysflow classes
     """
     _timeline = None
-    graphviz = Graphviz()
+    graphviz = visuals.Graphviz()
 
     @property
     def timeline(self):
@@ -18,16 +18,24 @@ class SimBase:
     def timeline(self, value):
         "Set timeline to this and all of the composed instances"
         self._timeline = value
-        for attr, attr_val in vars(self).items():
+        for sysflow_elem in self.sub_elements:
 
-            is_attr_simbase = isinstance(attr_val, SimBase)
-            
-            if is_attr_simbase:
-                
-                has_same_value = getattr(attr_val, '_timeline', None) is value
+            has_same_value = getattr(sysflow_elem, '_timeline', None) is value
+            if not has_same_value:
+                attr_val.timeline = value
 
-                if not has_same_value:
-                    attr_val.timeline = value
+    @property
+    def sub_elements(self):
+        "Return all SystemFlow elements from the object"
+        return {attr_val for attr, attr_val in vars(self).items() if isinstance(attr_val, SimBase)}
 
+    def reset_all(self):
+        "Reset all elements this element has"
+        self.reset()
+        for elem in self.sub_elements:
+            elem.reset()
 
-
+    def reset(self):
+        "Reset this element"
+        if hasattr(self, "has_memory") and self.has_memory:
+            raise NotImplementedError
